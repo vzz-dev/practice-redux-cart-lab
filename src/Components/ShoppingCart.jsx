38d09bdb238dfd89bf3ecf5474cad9e6b9e305bd
vzz-dev <workxoff>
2026-03-29 16:29:15 -0400

    terminando tarea SuperCoin

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -7,19 +7,46 @@ import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuanti
 const ShoppingCart = () => {
 
 const dispatch = useDispatch();
-  const cartItems = useSelector(state => state.cart.cartItems);
-  const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    const cartItems = useSelector(state => state.cart.cartItems);
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
 
-  return (
+const handleRemoveItem = itemId => {
+    dispatch(removeItemFromCart(itemId));
+};
+
+const handleClearCart = () => {
+    dispatch(clearCart());
+};
+
+const handleIncreaseItemQuantity = itemId => {
+    dispatch(increaseItemQuantity(itemId));
+};
+
+const handleDecreaseQuantity = itemId => {
+    dispatch(decreaseItemQuantity(itemId));
+};
+
+
+return (
     <>
-    <div className="shopping-cart">
-      <h2 className="shopping-cart-title">Shopping Cart</h2>
-      <ul className="cart-items">
-       
-      </ul>
-      <button className="clear-cart-btn">Clear Cart</button>
-    </div>
-  
+      <div className="shopping-cart">
+        <h2 className="shopping-cart-title">Carrito de Compras</h2>
+        <ul className="cart-items">
+          {cartItems.map(item => (
+            <li key={item.id} className="cart-item">
+              <span>{item.name} - ${item.price}</span>
+              <div className="quantity-controls">
+                <button onClick={() => handleDecreaseQuantity(item.id)}>-</button>
+                <span> {item.quantity}</span>
+                <button onClick={() => handleIncreaseQuantity(item.id)}>+</button>
+              </div>
+              <button className="remove-item-btn" onClick={() => handleRemoveItem(item.id)}>Eliminar</button>
+            </li>
+          ))}
+        </ul>
+        <button className="clear-cart-btn" onClick={handleClearCart}>Limpiar Carrito</button>
+      </div>
+      <div>{totalAmount ? <div>'El monto total es {totalAmount}</div> : ''}</div>
     </>
   );
 };
